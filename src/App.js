b321a69635327cf7b0ec9db7c3d6684c67b22ae9
vinicius <v.nunhez27@gmail.com>
2022-06-21 19:58:54 -0300

    Primeiro update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Home from './components/Home'
+import Produtos from './components/Produtos'
+import Sobre from './components/Sobre'
+import { BrowserRouter,Routes,Route,Link } from "react-router-dom";
+import {Nav} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Crud com React</h1> 
+  
+   <BrowserRouter>
+       <Nav variant="tabs">
+    <Nav.Link as={Link} to="/">Página Inicial</Nav.Link>
+    <Nav.Link as={Link} to="/produtos">Cadastro de Produtos</Nav.Link>
+    <Nav.Link as={Link} to="/sobre">Sobre</Nav.Link>
+        </Nav>
+      <Routes>
+        <Route path="/" index element={<Home/>}></Route>
+        <Route path="/produtos" element={<Produtos/>}></Route>
+        <Route path="/sobre" element={<Sobre/>}></Route>
+       </Routes>
+     </BrowserRouter>
     </div>
   );
 }
